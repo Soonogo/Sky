f86062ef324944c9f8941668e612cc6be15b470e
songen <tttsongen@foxmail.com>
2022-09-27 23:50:42 +0800

    优化apidoc的json

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -4,12 +4,11 @@ require 'rspec_api_documentation/dsl'
 resource "Orders" do
   post "/api/v1/validation_codes" do
     parameter :email,type: :string
-    header 'Content-type','application/json'
     let(:email){'1@qq.com'}
     example "发送验证码" do
       do_request
-
       expect(status).to eq 200
+      expect(response_body).to eq ' '
     end
   end
 end
