0b7f8d790e0d4deac532bf14a97ee83ed78da43a
songen <tttsongen@foxmail.com>
2022-09-27 23:12:40 +0800

    请求为json格式

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -3,10 +3,13 @@ require 'rspec_api_documentation/dsl'
 
 resource "Orders" do
   post "/api/v1/validation_codes" do
+    parameter :email,type: :string
+    header 'Content-type','application/json'
+    let(:email){'1@qq.com'}
     example "发送验证码" do
       do_request
 
-      expect(status).to eq 400
+      expect(status).to eq 200
     end
   end
 end
